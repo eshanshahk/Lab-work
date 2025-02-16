08fee7ad3795564e911340e7d3c1a288c9d2f6e3
eshanshahk <eshanshahk123@gmail.com>
2025-02-16 22:17:05 +0530

    created and executed program_03

---
+++ program_03.c
@@ -0,0 +1,21 @@
+// Write a C program to count number of words in a string
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int word = 0, count = 0;
+    printf("Enter a sentence");
+    scanf("%[^\n]", str);
+
+    while (str[word] != '\0')
+    {
+        if (str[word] == ' ')
+        {
+            count++;
+        }
+
+        word++;
+    }
+    printf("Number of words in this sentence are %d", count + 1);
+    return 0;
+}
