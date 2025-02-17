04d337d79c16efa431e8f2207c46089ec166cd97
eshanshahk <eshanshahk123@gmail.com>
2025-02-17 07:26:07 +0530

    created and started program_05(not executed)

---
+++ program_05.c
@@ -0,0 +1,10 @@
+//Write a program to check whether the string is a Palindrome without using string library functions.
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i;
+    printf("Enter a string to check whether it's palindrome or not! \n");
+    scanf("%s", str);
+
+    return 0;
+}
