8e84908ef5d352c68f2262094e02a405b66badf6
ESHAN SHAH K <eshanshahk123@gmail.com>
2025-02-18 10:49:13 +0530

    Update program_05.c adn executed successfully

--- program_05.c
+++ program_05.c
@@ -2,13 +2,23 @@
 #include <stdio.h>
 int main() {
     char str[100];
-    int i;
+    int i=0,len,j=0;
     printf("Enter a string to check whether it's palindrome or not! \n");
     scanf("%s", str);
-    if (str[i]==)
-    {
-        /* code */
+    while (str[i] != '\0') {
+        i++;
     }
-    
+        j=i-1;
+        i=0;
+    while(i<j){
+    	if(str[i]!=str[j])
+    	{
+    	    printf("The string is not palindrome");
+    	    return 0;
+        }
+        i++;
+        j--;  
+        }
+        printf("It is a palindrome");
     return 0;
 }
