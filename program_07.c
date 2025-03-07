2211b421021adb64b51c047b4d542427f5c9b620
eshanshahk <eshanshahk123@gmail.com>
2025-03-07 21:46:31 +0530

    finished program_07.c

--- program_07.c
+++ program_07.c
@@ -4,20 +4,25 @@
 
 int main()
 {
-    int arr[][],n;
-    printf("Enter the size of the array");
+    int arr[10][10], n;
+    printf("Enter the size of the array \n");
     scanf(" %d", &n);
     printf("Enter the array elements\n");
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             scanf(" %d", &arr[i][j]);
         }
     }
     printf("Entered array is \n");
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             printf(" %d", arr[i][j]);
-        }printf("\n");
+        }
+        printf("\n");
     }
     return 0;
 }
