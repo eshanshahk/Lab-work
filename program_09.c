cf98a4c5793f8b852c3502cd0eda7f5e81f8b465
ESHAN SHAH K <eshanshahk123@gmail.com>
2025-03-11 06:54:27 +0530

    Update program_09.c

--- program_09.c
+++ program_09.c
@@ -1,10 +1,15 @@
 // Write a recursive function to print the n th Fibonacci number
 #include <stdio.h>
 #include <stdlib.h>
-
+void fact(int);
+void fact(int a){
+    
+}
 int main()
 {
     int num;
-    printf();
+    printf(" Enter a number to find its factorial");
+    scanf(" %d", &num);
+    fact(num);
     return 0;
 }
