cf276ee071dd2b0878a31d0398acc0e3e98e55ef
eshanshahk <eshanshahk123@gmail.com>
2025-03-13 22:28:04 +0530

    finished program_12.c

--- program_12.c
+++ program_12.c
@@ -1,7 +1,24 @@
-//Write a function to swap two numbers using pointers 
+// Write a function to swap two numbers using pointers
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int x, y;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+
+    printf("After swapping: x = %d, y = %d\n", x, y);
+
     return 0;
 }
