27c3aecd5fc621febdb14a231609e05dbd9090f0
eshanshahk <eshanshahk123@gmail.com>
2025-03-13 21:23:51 +0530

    finished program_10.c

--- program_10.c
+++ program_10.c
@@ -1,7 +1,24 @@
-//Write a function to print nCr
+// Write a function to print nCr
 #include <stdio.h>
 #include <stdlib.h>
-
-int main() {
+int factorial(int n)
+{
+    int i, fact = 1;
+    for (i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+int main()
+{
+    int r, n, ncr;
+    printf("Let's find nCr! \n");
+    printf("Enter n : ");
+    scanf(" %d", &n);
+    printf("Enter r : ");
+    scanf(" %d", &r);
+    ncr = factorial(n) / (factorial(r) * factorial(n - r));
+    printf("%dC%d is %d", n, r, ncr);
     return 0;
 }
