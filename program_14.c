b092f9db5204db4030e47ce4800c70bfefba36d9
eshanshahk <eshanshahk123@gmail.com>
2025-03-13 06:51:25 +0530

    updated programs

--- program_14.c
+++ program_14.c
@@ -1,7 +1,20 @@
-//Write a C program to print the elements of an array in reverse order using pointers. 
+// Write a C program to print the elements of an array in reverse order using pointers.
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
+char rev(char a, int i)
+{
+    for (int *i; i > 0; i--)
+        printf("%s", a[i]);
+}
+int main()
+{
+    int i;
+    char str[20];
+    printf("Enter the string");
+    scanf("%s", str);
+    i = strlen(str);
+    printf("Reversed string is : %d", rev(str, i));
     return 0;
 }
