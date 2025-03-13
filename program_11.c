34bec61281913980ce209d3af94e3b0967695e0a
eshanshahk <eshanshahk123@gmail.com>
2025-03-13 22:10:22 +0530

    finished

--- program_11.c
+++ program_11.c
@@ -1,7 +1,25 @@
-//Write a recursive program to count the number of digits of a positive integer number?
-#include <stdio.h>
-#include <stdlib.h>
+// Recursive function to count digits
 
-int main() {
+#include <stdio.h>
+int countDigits(int n)
+{
+    if (n == 0)
+        return 0;
+    return 1 + countDigits(n / 10);
+}
+
+int main()
+{
+    int n;
+    printf("Enter a positive number: ");
+    scanf("%d", &n);
+
+    if (n < 0)
+    {
+        printf("Please enter a positive number.\n");
+        return 1;
+    }
+
+    printf("Number of digits in %d is %d\n", n, countDigits(n));
     return 0;
 }
